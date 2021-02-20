9b84745b87d949a099aacc18fa15854f6a5d60fa
Adam <Adamiss@users.noreply.github.com>
2021-02-20 19:28:05 +0100

    Update Appcss and navbar

--- FrontEnd/ppmtool-react-client/src/App.js
+++ FrontEnd/ppmtool-react-client/src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Dashboard />
     </div>
   );
