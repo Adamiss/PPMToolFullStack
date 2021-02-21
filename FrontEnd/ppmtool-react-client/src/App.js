5ae8982c9e10734967cc172907264ab7a5fd3304
Adam <Adamiss@users.noreply.github.com>
2021-02-21 09:12:43 +0100

    Create add project form

--- FrontEnd/ppmtool-react-client/src/App.js
+++ FrontEnd/ppmtool-react-client/src/App.js
@@ -2,14 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Layout/Header";
+import AddProject from "./components/Project/AddProject";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
+      </div>
+    </Router>
   );
 }
 
